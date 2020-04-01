463f8fe9253e1440329c3d14d59e479b6d30187c
Nikki Vaughan <vnikki13@gmail.com>
2020-04-01 12:48:03 -0700

    Finished implementing wave 1 with a drawLetters function. Passes wave 1 tests.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,49 @@
+const poolOfLetters = [
+  ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'],
+  ['B', 'B'],
+  ['C', 'C'],
+  ['D', 'D', 'D', 'D'],
+  ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+  ['F', 'F'],
+  ['G', 'G', 'G'],
+  ['H', 'H'],
+  ['I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I'],
+  ['J'],
+  ['K'],
+  ['L', 'L', 'L', 'L'],
+  ['M', 'M'],
+  ['N', 'N', 'N', 'N', 'N', 'N'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['P', 'P'],
+  ['Q'],
+  ['R', 'R', 'R', 'R', 'R', 'R'],
+  ['S', 'S', 'S', 'S'],
+  ['T', 'T', 'T', 'T', 'T', 'T'],
+  ['U', 'U', 'U', 'U'],
+  ['V', 'V'],
+  ['W', 'W'],
+  ['X'],
+  ['Y', 'Y'],
+  ['Z'],
+];
+
+const getrandNum = () => {
+  return Math.floor(Math.random() * 26)
+};
+
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
+    const userHand = [];
+
+    while (userHand.length < 10) {
+      let randNum = getrandNum();
+      let randLetter = poolOfLetters[randNum][0];
+      if (userHand.filter(x => x == randLetter).length < poolOfLetters[randNum].length) {
+        userHand.push(randLetter);
+      };
+    };
+
+    return userHand
   },
 };
 
