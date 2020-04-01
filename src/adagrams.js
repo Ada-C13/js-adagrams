f3312484f7b605934be51000a50f1ec954f17ebe
Jessica Liang <jessicursliang@gmail.com>
2020-04-01 15:41:01 -0700

    fixed logic for determining random number.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,16 +1,15 @@
 const Adagrams = {
   drawLetters() {
     const constLetterPool = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'G', 'G', 'G', 'H', 'H', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'J', 'K', 'L', 'L', 'L', 'L', 'M', 'M', 'N', 'N', 'N', 'N', 'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P', 'P', 'Q', 'R', 'R', 'R', 'R', 'R', 'R', 'S', 'S', 'S', 'S', 'T', 'T', 'T', 'T', 'T', 'T', 'U', 'U', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z'];
-    const playerHand = [];
+    let playerHand = [];
 
     for (let i = 0; i < 10; i++) {
-      const randomLetter = Math.random();
-
-      playerHand.push(constLetterPool[randomLetter]);
-    }
+      let randomNum = Math.floor(Math.random() * 100);
+      playerHand.push(constLetterPool[randomNum]);
+    };
 
     return playerHand;
-  },
+  }
 };
 
 // Do not remove this line or your tests will break!
