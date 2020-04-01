754128073f0176f3891ccfc3bcf2df2af94cada0
Lola Llanes <mslourdesllanes@gmail.com>
2020-04-01 12:05:35 -0700

    pseudo coded const allLetters, scoreChart, drawLetters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,22 @@
+const allLetters = {
+  //pool of available letters and the qty they each have
+}
+const scoreChart = {
+  // chart of letters and thier respective scores
+}
+
 const Adagrams = {
   drawLetters() {
     // Implement this method for wave 1
+    //empty array to hold drawn tiles
+    //var(let) to hold tile score
+
+    // user draws 10 tiles .random 
+    //let = to hold drawn tiles
+    //let = 
+    //empty array << drawn tiles
+    
+    //return drawn tiles
   },
 };
 
