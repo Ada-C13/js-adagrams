e819b040429a31480074bad2531a6d4c2b70aedc
Maryam Heshmati <mairheshmati@gmail.com>
2020-04-01 14:10:07 -0700

    finished wave 1- drawLetters function returns an array of 10 randomly selected letters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,51 @@
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
+    const letterValues = {
+      A: 9,
+      B: 2,
+      C: 2,
+      D: 4,
+      E: 12,
+      F: 2,
+      G: 3,
+      H: 2,
+      I: 9,
+      J: 1,
+      K: 1,
+      L: 4,
+      M: 2,
+      N: 6,
+      O: 8,
+      P: 2,
+      Q: 1,
+      R: 6,
+      S: 4,
+      T: 6,
+      U: 4,
+      V: 2,
+      W: 2,
+      X: 1,
+      Y: 2,
+      Z: 1,
+    };
+    //make an array containing all available letters 
+    let sample = Object.entries(letterValues).map(function ([key , value]){
+      return key.repeat(value).split('');
+    })
+
+    sample = sample.flat()
+
+    const lettersAtHand = []
+
+    for (let i = 0; i < 10; i++){
+      lettersAtHand.push(sample[Math.floor(Math.random()* sample.length)]);
+    };
+    return lettersAtHand  
   },
 };
+
+// Adagrams.drawLetters()
+
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
