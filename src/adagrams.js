da8f7252bc46201def7beb1d0014f867a26d7702
Corinna Fabre <corinna.fabre@gmail.com>
2020-04-01 15:31:44 -0700

    Updated notes for requirements.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,5 @@
 const Adagrams = {
-      // Our first job is to build a hand of 10 letters. To do so, add a function called drawLetters inside of the Adagrams object in src/adagrams.js. 
+      //ASSIGNMENT REQ: Our first job is to build a hand of 10 letters. To do so, add a function called drawLetters inside of the Adagrams object in src/adagrams.js. 
   drawLetters() {
     const alphabet = {
       A: 9, B: 2, C:2, D:4, E:12, F:2, G:3, H:2, I:9, J:1, K:1, L:4, M:2, N:6, O:8, P:2, Q:1, R:6, S:4, T:6, U:4, V:2, W:2, X:1, Y:2, Z:1
@@ -9,12 +9,13 @@ const Adagrams = {
 
     const only_letters = Object.keys(alphabet);
     
+    //ASSIGNMENT REQ: The letters should be randomly drawn from a pool of letters
     //Randomization adapted from from w3resource explanation of random approaches in JS: https://www.w3resource.com/javascript-exercises/javascript-array-exercise-35.php
     const random_item = (items) => {   
       return items[Math.floor(Math.random()*items.length)];    
     }//close random_item
 
-    //ASSIGNMENT REQ: 
+    //ASSIGNMENT REQ: Each string should contain exactly one letter
     for(let i = 0; i < 10; i++) {
       hand.push(random_item(only_letters));
 
@@ -32,13 +33,3 @@ const Adagrams = {
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
-
-// Returns an array of ten strings
-// Each string should contain exactly one letter
-// These represent the hand of letters that the player has drawn
-// The letters should be randomly drawn from a pool of letters
-// This letter pool should reflect the distribution of letters as described in the table below
-// There are only 2 available C letters, so drawLetters cannot ever return more than 2 Cs
-// Since there are 12 Es but only 1 Z, it should be 12 times as likely to draw an E as a Z
-// Invoking this function should not change the pool of letters
-// Imagine that the user returns their hand to the pool before drawing new letters
