aaf45afe26cfb2bbf282e48544c78c5e19d1bcd0
Nataliya Pogodina <n.pogodina@gmail.com>
2020-04-02 07:27:39 -0700

    Wave 1: added makeLetterPool() and drawLetters()

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,56 @@
 const Adagrams = {
+  letterQuantities: {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1
+  },
+
+  makeLetterPool() {
+    let letterPool = []; // let?
+    for (let letter in this.letterQuantities) {
+      for (let i = 0; i < this.letterQuantities[letter]; i++) {
+        letterPool.push(letter);
+      };
+    };
+    return letterPool;
+  },
+
   drawLetters() {
     // Implement this method for wave 1
+    let letterPool = this.makeLetterPool();
+
+    // Fisher-Yates Algorithm: https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
+    for (let i = letterPool.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = letterPool[i];
+      letterPool[i] = letterPool[j];
+      letterPool[j] = temp;
+    };
+    
+    return letterPool.slice(0, 10);
   },
 };
 
