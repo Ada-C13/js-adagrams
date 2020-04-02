6d942fd06d35778a92de4635efba6221bd5aa182
Nataliya Pogodina <n.pogodina@gmail.com>
2020-04-02 13:26:58 -0700

    Wave 3: added scoreWord()

--- src/adagrams.js
+++ src/adagrams.js
@@ -28,6 +28,35 @@ const Adagrams = {
     Z: 1
   },
 
+  letterValues: {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10
+  },
+
   makeLetterPool() {
     let letterPool = []; // let?
     for (let letter in this.letterQuantities) {
@@ -73,7 +102,18 @@ const Adagrams = {
     return true;
   },
 
-  
+  scoreWord(word) {
+    let totalScore = 0;
+    word.toUpperCase().split("").forEach((letter) => {
+      totalScore += this.letterValues[letter];
+    });
+
+    if (word.length > 6) {
+      totalScore += 8;
+    };
+
+    return totalScore;
+  }
 };
 
 // Do not remove this line or your tests will break!
