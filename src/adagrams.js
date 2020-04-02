0bc35d29d09544f5cf26c97c927c5739ebc86eb4
dnguye2 <dnguyen1093@gmail.com>
2020-04-02 16:02:28 -0700

    letter pool created

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,30 +1,56 @@
 // make letters and quantities as a hash
-let quantity = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1]
-
-const a = 65;
-const letterQuantity = {};
-for (let i = 0; i < 26; i++) {
-  letterQuantity[(String.fromCharCode(a + i))] = quantity[i]
+let letterQuantities = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1
 };
 
 const Adagrams = {
   drawLetters() {
     // Implement this method for wave 1
-    // method to create letter pool
-    // create a variable to hold letter pool, letterPool
-    // iterate through the hash and add it to the array, letterPool
-    // for each key and value, add the key to letterPool value amount of times
+    // for loop to create letter pool
+    letterPool = []
+    for (let [letter, quantity] of Object.entries(letterQuantities)) {
+      for(let i = 0; i < quantity; i++) {
+            letterPool.push(letter)
+          }
+    }
 
     // shuffle letters
-    // google ways to shuffle array in JS
+    // Fisher-Yates Algorithm - https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
+
 
     // draw letters
-    // draw 10 letters
+    // draw 10 letters - should be an array
+    // console log the 10 letters
   },
 };
 
 
 
 // Do not remove this line or your tests will break!
-export default Adagrams;
+// export default Adagrams;
 
