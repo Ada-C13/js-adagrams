1b519711fbd95e2d8b946b674d85573259364e57
Yaz <yasmineoshaugh@gmail.com>
2020-04-01 18:38:49 -0700

    scoreWord working

--- src/adagrams.js
+++ src/adagrams.js
@@ -53,6 +53,46 @@ const Adagrams = {
     }
     return true;
   },
+  scoreWord(word) {
+    let totalScore = 0;
+    word = word.toUpperCase();
+    const scores = {
+      A: 1,
+      N: 1,
+      B: 3,
+      O: 1,
+      C: 3,
+      P: 3,
+      D: 2,
+      Q: 10,
+      E: 1,
+      R: 1,
+      F: 4, 
+      S: 1,
+      G: 2, 
+      T: 1,
+      H: 4, 
+      U: 1,
+      I: 1, 
+      V: 4,
+      J: 8, 
+      W: 4,
+      K: 5, 
+      X: 8,
+      L: 1, 
+      Y: 4,
+      M: 3, 
+      Z: 10,
+    }
+    for(let i = 0; i < word.length; i++) {
+      const currentLetter = word[i];
+      totalScore += scores[currentLetter];
+    }
+    if (word.length > 6) {
+      totalScore += 8;
+    }
+    return totalScore;
+  },
 };
 
 // Do not remove this line or your tests will break!
