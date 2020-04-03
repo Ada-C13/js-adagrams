25249addf419eb031868296464c9bf420177ac6b
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-04-02 22:54:29 -0700

    wave 1

--- src/demo.js
+++ src/demo.js
@@ -1,7 +1,7 @@
-import Model from 'demo/model';
-import View from 'demo/view';
-import Controller from 'demo/controller';
+// import Model from 'demo/model';
+// import View from 'demo/view';
+// import Controller from 'demo/controller';
 
-// Initialize the controller I guess
-const game = new Controller(Model, View);
-game.start();
+// // Initialize the controller I guess
+// const game = new Controller(Model, View);
+// game.start();
