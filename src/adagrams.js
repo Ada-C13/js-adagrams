3ac529726f5c531eb4c9dd51d8e784d779083cab
cojenco <icathyouyang@gmail.com>
2020-04-02 21:01:58 -0700

    add scoreWord member function. complete wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,32 +1,3 @@
-// const letterQuantities = {
-//   'A': 9,
-//   'B': 2,
-//   'C': 2,
-//   'D': 4,
-//   'E': 12,
-//   'F': 2,
-//   'G': 3,
-//   'H': 2, 
-//   'I': 9,
-//   'J': 1,
-//   'K': 1,
-//   'L': 4,
-//   'M': 2,
-//   'N': 6,
-//   'O': 8,
-//   'P': 2,
-//   'Q': 1,
-//   'R': 6,
-//   'S': 4,
-//   'T': 6,
-//   'U': 4,
-//   'V': 2,
-//   'W': 2,
-//   'X': 1,
-//   'Y': 2,
-//   'Z': 1
-// };
-
 const Adagrams = {
   drawLetters() {
     const poolLetters = [];
@@ -36,32 +7,21 @@ const Adagrams = {
         poolLetters.push(letter);
       }
     }
-    console.log(poolLetters)
 
     let indexArray = Array.from(poolLetters.keys());
     const sampleIndex = this.getSample(10, indexArray);
-    // const sampleIndex = []
-    // let size = 10;
-    // for (let i = 1; i <= size; i++) {
-    //   const randomIndex = Math.floor(Math.random() * (indexArray.length - i));
-    //   sampleIndex.push(indexArray[randomIndex]);
-    //   indexArray[randomIndex] = indexArray[indexArray.length - i];
-    // };
-
     const lettersInHand = [];
     sampleIndex.forEach(index => lettersInHand.push(poolLetters[index]));
-    console.log(lettersInHand)
     return lettersInHand;
   },
+
   usesAvailableLetters(input, lettersInHand) {
     const splitInput = input.toUpperCase().split('')
     let tenLetters = this.arrayToObject(lettersInHand)
-    console.log(tenLetters)
     
     for (let char of splitInput) {
       if (tenLetters[char]) {
         tenLetters[char] -= 1;
-        console.log(tenLetters)
       } else {
         return false;
       }
@@ -73,6 +33,22 @@ const Adagrams = {
     // }
     return true;
   },
+
+  scoreWord(word) {
+    let score = 0;
+    const splitWord = word.toUpperCase().split('');
+
+    for (let char of splitWord) {
+      score += this.pointsPerLetter[char];
+    }
+
+    if (word.length > 6) {
+      score += 8;
+    }
+
+    return score;
+  },
+
   letterQuantities: {
     'A': 9,
     'B': 2,
@@ -101,6 +77,36 @@ const Adagrams = {
     'Y': 2,
     'Z': 1
   },
+
+  pointsPerLetter: {
+    'A': 1,
+    'B': 3,
+    'C': 3,
+    'D': 2,
+    'E': 1,
+    'F': 4,
+    'G': 2,
+    'H': 4, 
+    'I': 1,
+    'J': 8,
+    'K': 5,
+    'L': 1,
+    'M': 3,
+    'N': 1,
+    'O': 1,
+    'P': 3,
+    'Q': 10,
+    'R': 1,
+    'S': 1,
+    'T': 1,
+    'U': 1,
+    'V': 4,
+    'W': 4,
+    'X': 8,
+    'Y': 4,
+    'Z': 10
+  },
+
   getSample(total, inputArray) {
     const sampleArray = [];
     for (let i = 1; i <= total; i++) {
@@ -110,6 +116,7 @@ const Adagrams = {
     }
     return sampleArray;
   },
+
   arrayToObject(array) {
     const newObject = {}
     for (let element of array) {
