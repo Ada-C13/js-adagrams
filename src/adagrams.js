7a39dd34f775cbc0d044ca0c3fbb2b2ef41f10b2
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-03 02:14:40 -0700

    ensures a more weighted distribution

--- src/adagrams.js
+++ src/adagrams.js
@@ -4,7 +4,13 @@ const Adagrams = {
       'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
     }; // wave 3: make each key into an object containing scores and numbers available?
 
-    const lettersArr = Object.keys(LETTERS)
+    // this ensures a more weighted distribution
+    const lettersArr = [];
+    for (const key in LETTERS) {
+      for (let i=0; i<=LETTERS[key]; i++) {
+        lettersArr.push(key);
+      };
+    };
 
     const drawn = [];
     for (let i=0; i<10; i++) {
