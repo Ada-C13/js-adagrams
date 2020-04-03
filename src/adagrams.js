0b36f1122e3257cc44c4b1172ffe68950bb5b9bf
Rosalyn <syrosalynyu@gmail.com>
2020-04-02 20:59:02 -0700

    Completed scoreWord(word) function
    1.) Completed scoreWord(word) function
    2.) created a wordScores Object in the Adagrams Object

--- src/adagrams.js
+++ src/adagrams.js
@@ -28,6 +28,35 @@ const Adagrams = {
     Z: 1
   },
 
+  wordScores: {
+    A: 1, 
+    B: 3, 
+    C: 3, 
+    D: 2, 
+    E: 1, 
+    F: 4, 
+    G: 2, 
+    H: 4, 
+    I: 1, 
+    J: 8, 
+    K: 5, 
+    L: 1, 
+    M: 3, 
+    N: 1, 
+    O: 1, 
+    P: 3, 
+    Q: 10, 
+    R: 1, 
+    S: 1, 
+    T: 1, 
+    U: 1, 
+    V: 4, 
+    W: 4, 
+    X: 8, 
+    Y: 4, 
+    Z: 10
+  },
+
   // member function to draw 10 letters
   drawLetters() {
     let tenLetters = [];
@@ -75,6 +104,26 @@ const Adagrams = {
     };
     return array;
   },
+
+  scoreWord(word) {
+    let totalScore = 0;
+    const lettersArray = word.toUpperCase().split('');
+  
+    if (word.length === 0) {
+      return totalScore;
+    } else {
+      lettersArray.forEach( (letter) => {
+        totalScore += this.wordScores[letter];
+      })
+    } 
+    
+    if (word.length >= 7 && word.length <= 10) {
+      totalScore += 8
+    }
+    
+    return totalScore;
+  },
+
 };
 
 // Do not remove this line or your tests will break!
