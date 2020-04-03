5f92baaafdbebcd98e06dafec27ad41710f4bb6d
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-04-02 18:01:31 -0700

    Added drawLetters function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,57 @@
+// Randomize array in-place using Durstenfeld shuffle algorithm
+const shuffleArray = function(array) {
+  for (var i = array.length - 1; i > 0; i --) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
 const Adagrams = {
   drawLetters() {
     // Implement this method for wave 1
+    const letterQuantities = {
+      "A": 9,
+      "B": 2,
+      "C": 2,
+      "D": 4,
+      "E": 12,
+      "F": 2,
+      "G": 3,
+      "H": 2,
+      "I": 9,
+      "J": 1,
+      "K": 1,
+      "L": 4,
+      "M": 2,
+      "N": 6,
+      "O": 8,
+      "P": 2,
+      "Q": 1,
+      "R": 6,
+      "S": 4,
+      "T": 6,
+      "U": 4,
+      "V": 2,
+      "W": 2,
+      "X": 1,
+      "Y": 2,
+      "Z": 1
+    }
+    let letterPool = [];
+    // put each letter.value times into pool
+    for(const letter in letterQuantities) {
+      for(let i = letterQuantities[letter]; i > 0; i --) {
+        letterPool.push(letter);
+      }   
+    }
+    // shuffle
+    const shuffled = shuffleArray(letterPool.slice(0));
+    // hand of 10 letters
+    const hand = shuffled.slice(0, 10);
+    return hand;
   },
 };
 
--- src/demo.js
+++ src/demo.js
@@ -5,3 +5,5 @@ import Controller from 'demo/controller';
 // Initialize the controller I guess
 const game = new Controller(Model, View);
 game.start();
+
+// making the demo game work is optional-- passing the unit tests is mandatory
