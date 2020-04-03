a8d3bcddc9ab52bc398e3763072adb45811e6610
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-03 01:36:41 -0700

    simplifies lettersArr, uses LETTERS object to determine letter availabilty, passes wave 1 tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -2,22 +2,27 @@ const Adagrams = {
   drawLetters() {
     const LETTERS = {
       'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
-    };
+    }; // wave 3: make each key into an object containing scores and numbers available?
 
-    lettersArray = [];
-    for (const key in LETTERS) {
-      for (let i=0; i<=LETTERS.key; i++) {
-        lettersArray.push(key);
+    const lettersArr = Object.keys(LETTERS)
+
+    const drawn = [];
+    for (let i=0; i<10; i++) {
+      let letter = lettersArr[Math.floor(Math.random()*lettersArr.length)];
+
+      // if the number available in the pool is less than 1, a new random value is assigned to letter
+      while (LETTERS[letter] < 1) {
+        letter = lettersArr[Math.floor(Math.random()*lettersArr.length)];
       };
-    };
 
-    const drawn = {};
-    for (let i=0; i<=10; i++) {
-      lettersArray[Math.floor(Math.random()*lettersArray.length)];
-      // iterate 10 times: draw 10 letters at random, increasing the value by 1 each time a letter is added, and reducing the number in POOL_OF_LETTERS by one each time a letter is drawn. If the number available in the pool is less than 1, it cannot be drawn.
+      // if the letter is available, it is added to the hand and the number available in the pool is decremented 
+      while (LETTERS[letter] >= 1) {
+        drawn[i] = letter;
+        LETTERS[letter] -= 1;
+      };
     }
-
-    // return Objects.keys(drawn) because the tests are expecting an array!
+    
+    return drawn;
   },
 };
 
