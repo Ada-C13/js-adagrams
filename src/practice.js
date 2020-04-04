9ff0e9a70e5b462e6aa64b85f08d82859d09daf0
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-04-04 14:44:36 -0700

    updated apporach for hash to array

--- src/practice.js
+++ src/practice.js
@@ -1,4 +1,5 @@
-const poolOfLetters = {
+let poolOfLetters = [];
+const distroOfLetters = {
     A: 9,
     B: 2,
     C: 2,
@@ -26,26 +27,86 @@ const poolOfLetters = {
     X: 1,
     Y: 2,
     Z: 1,
-    Z: 1
+    Z: 1,
 };
-// // console.log(poolOfLetters);
 
-Object.keys(poolOfLetters).forEach(function(item) {
-    letterStrings = item.repeat(poolOfLetters[item])
-        // console.log(letterBank); // key
-        // console.log(poolOfLetters[item]); // value
-    const str = letterStrings; // string of letters
-    const arr = str.split(''); // may need a loop for value
-    for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
-        const distro = arr[i]
-        console.log(distro);
+for (const letter in distroOfLetters) {
+    if (distroOfLetters != undefined) {
+        const letterCount = distroOfLetters[letter];
+        const arrayOfLetters = letter.repeat(letterCount).split("");
+        poolOfLetters = poolOfLetters.concat(arrayOfLetters);
     }
-});
+}
+// function addNums(num1, num2) {
+//     return num1 + num2;
+// }
+function poolOfLettersShuffle() {
+    let newPosition, temp;
+    for (let i = poolOfLetters.length - 1; i > 0; i--) {
+        newPosition = Math.floor(Math.random() * (i + 1));
+        temp = poolOfLetters[i];
+        poolOfLetters[i] = poolOfLetters[newPosition];
+        poolOfLetters[newPosition] = temp;
+    }
+    return poolOfLetters;
+}
 
+const shuffledLettersArray = poolOfLettersShuffle();
+console.log(shuffledLettersArray);
 
+function handOfletters() {
+    let shuffle = 1;
+    while (shuffle < 10) {
+        console.log(shuffledLettersArray.pop());
+        shuffle += 1;
+    }
+}
+console.log(handOfletters());
 
+// var n = 0;
+// var x = 0;
+// let arr = ["a", "b", "c"];
+// while (n < 3) {
+//     n++;
 
+//     console.log($ { arr } + n);
+// }
+
+// Object.keys(poolOfLetters).forEach(function(item) {
+//     letterStrings = item.repeat(poolOfLetters[item]);
+//     // console.log(letterBank); // key
+//     // console.log(poolOfLetters[item]); // value
+//     const str = letterStrings; // string of letters
+//     const arr = str.split(""); // may need a loop for value
+//     const bank = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         // console.log(arr[i]);
+//         const letterArray = [];
+//         const letter = arr[i];
+//         letterArray.push(letter);
+//         // console.log(letterArray);
+//         bank.push(letterArray);
+//         // for (let i = 0;  < letterArray.length; i++) {
+//         //     const bank = [];
+//         //     const eachletterArray = letterArray[i];
+//         //     bank.push(eachletterArray);
+//         //     console.log(bank);
+//         // }
+//     }
+//     console.log(bank);
+
+// console.log(letterBank.join());
+// const bank = [];
+// letterStrip = letterBank.join();
+// bank.push(letterStrip);
+// console.log(bank);
+
+// for (let i = 0; i < letterBank.length; i++) {
+//     const letters = letterBank[i];
+//     letterBank.push(distro);
+// }
+// console.log(distro);
+// });
 
 // let myShows = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men',
 //     'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter'
@@ -55,31 +116,9 @@ Object.keys(poolOfLetters).forEach(function(item) {
 
 // console.log(show)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // letters = Object.keys(poolOfLetters)
 // letterCount = Object.values(poolOfLetters)
 // letterBank = Object.entries(poolOfLetters)
-
 
 // console.log(letterCount)
 
