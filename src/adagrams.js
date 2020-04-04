e19d4bb02a3b1630e57b7ea914958d33e687c85a
Halahaddad1 <h.m.haddad@gmail.com>
2020-04-03 23:13:30 -0700

    finished wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -11,11 +11,55 @@ const Adagrams = {
       // let randomNumber = Math.floor(Math.random()*letterPool.length);
       let gamePool = [];
       for(let i= 0; i < 10 ; i++){
-      gamePool[i] = letterPool[Math.floor(Math.random()*letterPool.length)];
+      gamePool[i] = letterPool[Math.floor(Math.random()*letterPool.length)].toUpperCase();
       }
       return gamePool;
       console.log(gamePool);
     },
+    usesAvailableLetters(str,arr){
+    let wordArr = str.split("");
+    console.log(wordArr);
+    console.log(arr);
+    let count= 0;
+    for (let i=0; i<wordArr.length; i++){
+      arr.includes(wordArr[i])? arr.splice(arr.indexOf(wordArr[i]),1): count +=1 ;
+    }
+    console.log(arr);
+    if (arr.length + wordArr.length ===10 && count === 0){
+    return true;
+    } else {
+    return false;
+    }
+  },
+  scoreWord(str){
+    let score = 0;
+    let strup = str.toUpperCase();
+    let wordArr = strup.split('');
+    for (let i = 0; i< wordArr.length; i++){
+      const letter = wordArr[i];
+        if (["A","E","I","O","U","L","N","R","S","T"].includes(letter)){
+          score += 1;
+        }else if (["D","G"].includes(letter)){
+          score += 2;
+        }else if (["B","C","M","P"].includes(letter)){
+          score += 3;
+        }else if (["F","H","V","W","Y"].includes(letter)){
+          score += 4;
+        }else if (["K"].includes(letter)){
+          score += 5;
+        }else if (["J","X"].includes(letter)){
+          score += 8;
+        }else if (["Q","Z"].includes(letter)){
+          score += 10;
+        }
+      }
+      if (wordArr.length>=7){
+      return score+8;
+    }else {
+      return score;
+    }
+  }
+  
 };
 
 console.log(Adagrams.drawLetters());
