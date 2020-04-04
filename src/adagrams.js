a47782f4273bba271682d24297f3de3db95269ac
SuelyBarreto <suely@barreto.us>
2020-04-04 09:36:38 -0700

    Wave 3 - scoreWord(word)

--- src/adagrams.js
+++ src/adagrams.js
@@ -6,7 +6,7 @@ const Adagrams = {
                          "O": 8, "P": 2, "Q": 1, "R": 6, "S":  4, "T": 6, "U": 4,
                          "V": 2, "W": 2, "X": 1, "Y": 2, "Z": 1 };
     let letterPool = "";
-    for (const [letter, value] of Object.entries(letterDist)) {
+    for (const [letter, value] of Object.entries(letterDist)) { // Object.entries({a: 1, b: 2}) ==> [ [ 'a', 1 ], [ 'b', 2 ] ]
       letterPool += letter.repeat(value);
     }
     let hand = [];
@@ -16,9 +16,9 @@ const Adagrams = {
       // console.log(i, randIndex, randLetter);
       hand.push(randLetter);
     }
-
     return hand;
   },
+  
   // Method to check if the word is an anagram of some or all of the given letters in the hand.
   usesAvailableLetters(input, lettersInHand) {
     let handCopy = Array.from(lettersInHand);
@@ -32,43 +32,30 @@ const Adagrams = {
     }
     return true;
   },
+
+  // Method that returns the score of a given word as defined by the Adagrams game
   scoreWord(word) {
-    return 1;
-  }
+    const letterValues = { "A": 1, "B": 3, "C":  3, "D": 2, "E":  1, "F": 4, "G": 2,
+                           "H": 4, "I": 1, "J":  8, "K": 5, "L":  1, "M": 3, "N": 1,
+                           "O": 1, "P": 3, "Q": 10, "R": 1, "S":  1, "T": 1, "U": 1,
+                           "V": 4, "W": 4, "X":  8, "Y": 4, "Z": 10 };
+    word = word.toUpperCase()
+    let score = 0;
+    for(let i = 0; i < word.length; i++) {
+      score += letterValues[word[i]];
+    }
+    if (word.length >= 7 && word.length <= 10) score += 8;
+    return score;
+  },
+
+  
+
 }
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
-
-/* A method that returns the score of a given word as defined by the Adagrams game.
-def score_word(word)
-  letter_values = word.upcase.split("").map do |letter|
-    case letter
-      when "A", "E", "I", "O", "U", "L", "N", "R", "S", "T"
-        1
-      when "D", "G"
-        2
-      when "B", "C", "M", "P"
-        3
-      when "F", "H", "V", "W", "Y"
-        4
-      when "K"
-        5
-      when "J", "X"
-        8
-      when "Q", "Z"
-        10
-    end
-  end
-  
-  if word.length >= 7 && word.length <= 10
-    letter_values << 8
-  end
-  
-  return letter_values.sum
-end
-
-# A method that looks at the array of words and calculates which of these words has the highest score.
+ 
+/* # A method that looks at the array of words and calculates which of these words has the highest score.
 def highest_score_from(words)
   maximum_score = words.map { |word| score_word(word) }.max
   highest = words.select { |word| score_word(word) == maximum_score }
