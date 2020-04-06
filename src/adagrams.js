d232b5f0d4da9a2780396297293766585d68868b
Hannah <hannahjoo24@gmail.com>
2020-04-05 22:55:26 -0700

    Changed a variable name from letters_pool to lettersPool

--- src/adagrams.js
+++ src/adagrams.js
@@ -2,7 +2,7 @@
 
 class Adagrams {
   constructor() {
-    this.letters_pool = {
+    this.lettersPool = {
       A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1
     };
 
@@ -20,10 +20,10 @@ class Adagrams {
   // Implement this method for wave 1
   drawLetters() {
     const hand = [];
-    const letters_pool = this.letters_pool;
+    const lettersPool = this.lettersPool;
  
-    for (const letter in letters_pool) {
-      for (let i = 0; i < letters_pool[letter]; i++) {
+    for (const letter in lettersPool) {
+      for (let i = 0; i < lettersPool[letter]; i++) {
         hand.push(letter);
       };
     };   
@@ -91,7 +91,6 @@ class Adagrams {
         };
       };
     };
-
     return score;
   };
 
