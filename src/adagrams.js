f68f7406170dce6116e0acd6738e51fc5d54a789
Katie <katie.vandervoort@gmail.com>
2020-04-06 15:34:32 -0700

    started implementing drawLetters function by creating letterDist object and a letterPool array with all of the letters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,47 @@
 const Adagrams = {
-  drawLetters() {
-    // Implement this method for wave 1
+  drawLetters: function() {
+    const letterDist = {
+      A : 9, 
+      B : 2,  
+      C : 2,  
+      D : 4,  
+      E : 12, 
+      F : 2,  
+      G : 3,  
+      H : 2, 
+      I : 9,
+      J : 1, 
+      K : 1, 
+      L : 4,  
+      M : 2,  
+      N : 6,
+      O : 8,
+      P : 2,
+      Q : 1,
+      R : 6,
+      S : 4,
+      T : 6,
+      U : 4,
+      V : 2, 
+      W : 2,
+      X : 1,
+      Y : 2,
+      Z : 1
+    };
+    let letterPool = []
+    // create letterPool
+    for (const letter in letterDist) {
+      for (let i = 0; i < letterDist[letter]; i++) {
+        letterPool.push(letter);
+      } 
+    }
+
   },
+
+
 };
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
+
+// console.log(Adagrams.drawLetters())
