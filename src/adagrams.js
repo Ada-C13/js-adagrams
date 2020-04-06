b709b284eb4825500add992b141f87424d9ecd80
Haben <haben7@gmail.com>
2020-04-05 22:28:57 -0700

    all tests are passing & also implemented wave 5

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,9 @@
-const Adagrams = {
+class Adagrams {
   // wave 1
   drawLetters() {
     const sample = new Map([['A', 9], ['B', 2], ['C', 2], ['D', 4], ['E', 12], ['F', 2], ['G', 3], ['H', 2], ['I', 9], ['J', 1], ['K', 1], ['L', 4], ['M', 2], ['N', 6], ['O', 8], ['P', 2], ['Q', 1], ['R', 6], ['S', 4], ['T', 6], ['U', 4], ['V', 2], ['W', 2], ['X', 1], ['Y', 2], ['Z', 1]])
     const poolLetters = []
+
     for (let [letter, quantity] of sample) {
       for (let i = 0; i < quantity; i++) {
         poolLetters.push(letter)
@@ -10,35 +11,33 @@ const Adagrams = {
     }
     //randomize the pool of letters
     const theHand = []
+
     for (let i = 0; i < 10; i++) {
-      let index = Math.random() * (poolLetters.length - 1); //'ll give us numbers from 0- length
+      let index = Math.random() * (poolLetters.length - 1); // 'll give us numbers from 0- length
       let letter = poolLetters[Math.round(index)];
       theHand.push(letter)
     }
-    return theHand
-  },
+    return theHand;
+  }
 
   // wave 2
   usesAvailableLetters(input, lettersInHand) {
     let givenLetters = input.split('');
     let selectedLetters = lettersInHand.slice();
+
     for (let letter of givenLetters) {
       if (selectedLetters.includes(letter)) {
         let letterToFind = selectedLetters.indexOf(letter);
         selectedLetters.splice(letterToFind, 1);
-        //console.log(selectedLetters)
       }
       else {
         return false;
       }
     }
     return true;
-  },
+  }
+
   // wave 3
-  // Returns an integer representing the number of points
-  // Each letter within word has a point value. The number 
-  // of points of each letter is summed up to represent the 
-  // total score of word
   scoreWord(word) {
     const score = {
       A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
@@ -60,26 +59,20 @@ const Adagrams = {
     };
     return totalScore;
 
-  },
+  }
 
   // wave 4
-  //Add a function named highestScoreFrom 
   highestScoreFrom(words) {
-    //Has one parameter: words, which is an array of strings
-
-
-    // loop through each word and find the score each word
     let highestScore = -1;
     let highestScoredWord = "";
+
     for (let i = 0; i < words.length; i++) {
       let currentWord = words[i];
       let currentScore = this.scoreWord(currentWord);
-      console.log(currentWord + " " + currentScore)
       if (currentScore > highestScore) {
         highestScore = currentScore;
         highestScoredWord = currentWord
       }
-      // else if the currentScore is === to the highestScore
       else if (currentScore === highestScore) {
         // tie breaker rule:
         // compare the currentWord to the highestScoredWord
@@ -92,7 +85,7 @@ const Adagrams = {
       }
     }
 
-    // return an object of highestScoredWord and highestScore  
+    // return the winning word(highestScoredWord) and its score(highestScore) 
     return { score: highestScore, word: highestScoredWord };
 
   }
