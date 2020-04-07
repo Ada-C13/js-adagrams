36eaaf4459e36fe5b12a0606ec0da91ee40f8990
Angela Nguyen <angethuy@gmail.com>
2020-04-07 00:23:06 -0700

    draw letters and uses available functions now working

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,28 @@
 const Adagrams = {
+  lettersPool: ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z",],
   drawLetters() {
-    // Implement this method for wave 1
+    const hand = []
+    while (hand.length < 10) {
+      let tileNumber = Math.round(Math.random() * 98);
+      if (!hand.includes(tileNumber)) {
+        hand.push(this.lettersPool[tileNumber]);
+      }
+    }
+    return hand;
+  },
+
+  usesAvailableLetters(input, drawnHand) {
+    const hash = {}
+
+    for (let i = 0; i < drawnHand.length; i++) {
+      hash[drawnHand[i]] != null ? hash[drawnHand[i]] += 1 : hash[drawnHand[i]] = 1
+    };
+
+    for (let i = 0; i < input.length; i++) {
+      if (hash[input[i]] === undefined || hash[input[i]] === 0) return false;
+      hash[input[i]]--;
+    }
+    return true;
   },
 };
 
