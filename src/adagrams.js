31852f4a9ea1c135c8706c070e62b09393c65220
mulhoo <oliviamulholland94@gmail.com>
2020-04-06 22:16:51 -0700

    drawLetters failing, reworking

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,120 +1,120 @@
 const letterBank = {
   A: {
     quantity: 9,
-    score: 1
+    letterValue: 1
   },
   B: {
     quantity: 2,
-    score: 3
+    letterValue: 3
   },
   C: {
     quantity: 2,
-    score: 3
+    letterValue: 3
   },
   D: {
     quantity: 4,
-    score: 2
+    letterValue: 2
   }, 
   E: {
     quantity: 12,
-    score: 1
+    letterValue: 1
   },
   F: {
     quantity: 2,
-    score: 3
+    letterValue: 3
   },
   G: {
     quantity: 3,
-    score: 2
+    letterValue: 2
   },
   H: {
     quantity: 2,
-    score: 4
+    letterValue: 4
   },
   I: {
     quantity: 9,
-    score: 1
+    letterValue: 1
   },
   J: {
     quantity: 1,
-    score: 8
+    letterValue: 8
   },
   K: {
     quantity: 1,
-    score: 5
+    letterValue: 5
   },
   L: {
     quantity: 4,
-    score: 1
+    letterValue: 1
   },
   M: {
     quantity: 2,
-    score: 3
+    letterValue: 3
   },
   N: {
     quantity: 6,
-    score: 1
+    letterValue: 1
   },
   O: {
     quantity: 8,
-    score: 1
+    letterValue: 1
   },
   P: {
     quantity: 2,
-    score: 3
+    letterValue: 3
   },
   Q: {
     quantity: 1,
-    score: 10
+    letterValue: 10
   },
   R: {
     quantity: 6,
-    score: 1
+    letterValue: 1
   },
   S: {
     quantity: 4,
-    score: 1
+    letterValue: 1
   },
   T: {
     quantity: 6,
-    score: 1
+    letterValue: 1
   },
   U: {
     quantity: 4,
-    score: 1
+    letterValue: 1
   },
   V: {
     quantity: 2,
-    score: 4
+    letterValue: 4
   },
   W: {
     quantity: 2,
-    score: 4
+    letterValue: 4
   },
   X: {
     quantity: 1,
-    score: 8
+    letterValue: 8
   },
   Y: {
     quantity: 2,
-    score: 4
+    letterValue: 4
   },
   Z: {
     quantity: 1,
-    score : 10
+    letterValue: 10
   },
 };
 
 const Adagrams = {
   drawLetters() {
-    let lettersDeck = []
+    const lettersDeck = [];
 
     for (let key in letterBank) {
       while (letterBank[key]['quantity'] > 0) {
         lettersDeck.push(key);
         letterBank[key]['quantity'] -= 1;
-      };
-    };
+      }
+    }
 
     lettersDeck.sort(() => Math.random() - 0.5);
     return lettersDeck.slice(0, 10);
@@ -143,29 +143,46 @@ const Adagrams = {
   },
 
   scoreWord(word) {
-    let score = 0;
+    let wordScore = 0;
 
     for (let i =0; i < word.length; i++) {
       let upcaseLetter = word.toUpperCase();
-      score += letterBank[upcaseLetter[i]]['score'];
-    };
+      wordScore += letterBank[upcaseLetter[i]]['letterValue'];
+    }
 
     if (word.length >= 7) {
-      score += 8;
-    };
-    
-    return score
+      wordScore += 8;
+    }
+  
+    return wordScore;
   },
 
-  highestScoreFrom (words) {
+  highestScoreFrom(words) {
     let highScore = {
       word: '',
       score: 0
-    }
-    
-    const all_score = this.scoreWord(word)
-  }
+    };
 
+    words.forEach(word => {
+      const score = this.scoreWord(word);
+
+      if (score > highScore['score']) {
+        highScore['score'] = score;
+        highScore['word'] = word;
+      } else if (score == highScore['score']) {
+        if (word.length == 10 && highScore['word'].length != 10) {
+          highScore['word'] = word;
+        } else if (
+          word.length < highScore['word'].length &&
+          highScore['word'].length != 10
+        ) {
+          highScore['word'] = word;
+        }
+      }
+    });
+
+   return highScore;
+  }
 };
 
 export default Adagrams;
