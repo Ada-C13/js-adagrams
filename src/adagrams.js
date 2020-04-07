367306a0798a8489fb52b0ddff004d6f68b4b4a6
SuelyBarreto <suely@barreto.us>
2020-04-06 20:13:08 -0700

    fixed indentation, improved comments, and minor fixes

--- src/adagrams.js
+++ src/adagrams.js
@@ -15,22 +15,22 @@ class Adagrams {
     }
 
     // Create a hand by drawing random letters from the letter pool
-    let hand = [];
+    const hand = [];
     for (let i = 0; i < handSize; i++) {
       let randIndex  = Math.floor(Math.random() * letterPool.length);
       let randLetter = letterPool.slice(randIndex, randIndex + 1);
       hand.push(randLetter);
-    }
+    } 
     return hand;
   }
   
   // Method to check if the word is an anagram of some or all of the given letters in the hand
   static usesAvailableLetters(input, lettersInHand) {
     // Make a copy of the hand so we don't mess with the original one
-    let handCopy = Array.from(lettersInHand);
+    const handCopy = [...lettersInHand];
     // Loop thru the word/input to make sure every letter is in the hand
     for(let i = 0; i < input.length; i++) {
-      let letter = input[i];
+      const letter = input[i];
       // Check if the letter is in the hand
       if(handCopy.includes(letter)) {
         // if the letter is in the hand, remove from the hand
@@ -49,35 +49,46 @@ class Adagrams {
                            "H": 4, "I": 1, "J":  8, "K": 5, "L":  1, "M": 3, "N": 1,
                            "O": 1, "P": 3, "Q": 10, "R": 1, "S":  1, "T": 1, "U": 1,
                            "V": 4, "W": 4, "X":  8, "Y": 4, "Z": 10 };
-    word = word.toUpperCase()
+    const upperCaseWord = word.toUpperCase();
     // Loop thru the word and add the value for each letter
     let score = 0;
-    for(let i = 0; i < word.length; i++) {
-      score += letterValues[word[i]];
+    for(let i = 0; i < upperCaseWord.length; i++) {
+      score += letterValues[upperCaseWord[i]]; 
     }
-    if (word.length >= 7 && word.length <= 10) score += 8;
+    if (upperCaseWord.length > 6) {
+      score += 8;
+    } 
     return score;
   }
 
-  // Method that looks at the array of words and calculates which of these words has the highest score
+  // Method that looks at the array of words and
+  // calculates which of these words has the highest score
   static highestScoreFrom(words) {
     // Find the maximum score from all the words
-    let maximumScore = Math.max(...words.map(word => this.scoreWord(word)));
+    const maximumScore = Math.max(
+      ...words.map(word => this.scoreWord(word))
+    );
     // Find the words that have the maximum score
-    let highestWords = words.filter(word => this.scoreWord(word) === maximumScore);
+    const highestWords = words.filter(
+      word => this.scoreWord(word) === maximumScore
+    );
     // If there is only one word with the maximum score, return it
     if (highestWords.length === 1) {
-      return {word: highestWords[0], score: maximumScore};
+      return { word: highestWords[0], score: maximumScore };
     }
-
     // Find the minimum length from all the words with the maximum score
-    let minimumLength = Math.min(...highestWords.map(word => word.length == 10 ? 0 : word.length));
+    const minimumLength = Math.min(
+      ...highestWords.map(
+        word => word.length === 10 ? 0 : word.length
+      )
+    );
     // Find the words that have the minimum length
-    let shortestWords = highestWords.filter(word => (word.length == 10 ? 0 : word.length) === minimumLength);
-    return {word: shortestWords[0], score: maximumScore};
+    const shortestWords = highestWords.filter(
+      word => (word.length === 10 ? 0 : word.length) === minimumLength
+    );
+    return { word: shortestWords[0], score: maximumScore };
   }
 }
 
-// Do not remove this line or your tests will break!
 export default Adagrams;
  
